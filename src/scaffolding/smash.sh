f2bf910cc49e44daf3d07d20dc6d204d86b744f7
Adam Barth <github@adambarth.com>
2016-07-29 10:22:10 -0700

    Update smash.sh

--- src/scaffolding/smash.sh
+++ src/scaffolding/smash.sh
@@ -6,7 +6,7 @@ FUCHSIA_ROOT=/vagrant/src/fuchsia
 TARGET_PLATFORM=x86_64-fuchsia
 SDK=$FUCHSIA_ROOT/buildtools/sdk
 SYSROOT=$SDK/platforms/$TARGET_PLATFORM
-BUILT_SYSROOT=$FUCHSIA_ROOT/magenta/build-magenta-qemu-x86-64/sysroot
+BUILT_SYSROOT=$FUCHSIA_ROOT/magenta/build-magenta-pc-x86-64/sysroot
 
 cp $BUILT_SYSROOT/lib/libc.a $SYSROOT/lib/libc.a
 cp -rT $BUILT_SYSROOT/include $SYSROOT/include
